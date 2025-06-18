db483e20645298c4da3e3d243e4c151c26758e6a
Shreevatsa R <shreevatsa.public@gmail.com>
2025-06-17 20:50:12 -0700

    Add comments to doit.sh

--- doit.sh
+++ doit.sh
@@ -4,13 +4,18 @@ ORIG=../gp-mbh/unabridged.pdf
 # ORIG=../page-200/out-mbh-mutool.pdf
 HELPER=helper_fonts/NotoSansDevanagari-Regular.ttx
 
+# cd into a tmpdir and extract fonts (.ttf files) from the PDF file ($ORIG).
 tmpdir=tmp-$(date +%s)
 echo "Creating ${tmpdir}"
 mkdir -p ${tmpdir}
 cd ${tmpdir}
 mutool extract ../${ORIG}
 rm image-*.{png,jpg} || true
+
+# Dump text runs from the file
 RUST_BACKTRACE=1 cargo run --release --bin dump-tjs -- ../${ORIG}
+
+# Dump glyphs from the file. Then create and open HTML file with sample runs for each glyph.
 for f in *.ttf; do
     echo $f
     RUST_BACKTRACE=1 cargo run --release --bin dump-glyphs -- $f
